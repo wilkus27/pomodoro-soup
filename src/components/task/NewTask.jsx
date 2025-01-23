efec413c2b4c61e4156dda89f19a5ee3ce4fd5d0
wilkus27 <natalia.wilkus@openstax.pl>
2025-01-23 10:31:58 +0100

    create tasks list

--- src/components/task/NewTask.jsx
+++ src/components/task/NewTask.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import NewTaskCard from './NewTaskCard';
 import AddTaskButton from './AddTaskButton';
 
-export default function NewTask() {
+export default function NewTask( {change, submit, pomodoros, setPomodoros} ) {
     const [addTaskButton, setAddTaskButton] = useState(true);
 
     const showAddTaskButton = () => {
@@ -15,7 +15,17 @@ export default function NewTask() {
 
     return (
         <div className="new-task">
-            {addTaskButton ? <AddTaskButton hideButton={hideAddTaskButton} /> : <NewTaskCard showAddTaskButton={showAddTaskButton} />}
+            {addTaskButton ? (
+                <AddTaskButton hideButton={hideAddTaskButton} />
+            ) : (
+                <NewTaskCard 
+                    showAddTaskButton={showAddTaskButton} 
+                    change={change} 
+                    submit={submit}
+                    pomodoros={pomodoros}
+                    setPomodoros={setPomodoros}
+                />
+            )}
         </div>
     )
 }
--- src/components/task/TasksContainer.jsx
+++ src/components/task/TasksContainer.jsx
@@ -1,12 +1,72 @@
+import { useEffect, useState } from "react";
 import NewTask from "./NewTask";
 
 export default function TasksContainer() {
+  const [newTask, setNewTask] = useState("");
+
+  const [tasks, setTasks] = useState(() => {
+    const savedTasks = localStorage.getItem("tasks");
+    if (savedTasks) {
+      return JSON.parse(savedTasks);
+    } else {
+      return [];
+    }
+  });
+
+  const [pomodoros, setPomodoros] = useState(1);
+
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
+
+  function handleNewTaskChange(event) {
+    setNewTask(event.target.value);
+  }
+
+  function handleNewTaskSubmit() {
+    setTasks((currentTasks) => {
+        return [...currentTasks, {
+          id: crypto.randomUUID(),
+          name: newTask,
+          pomodoros: pomodoros,
+          completed: false
+        }]
+      }
+    )
+
+    setPomodoros(1);
+  }
+
   return (
     <div className="tasks-container">
       <div className="tasks-top-panel">
         <span className="tasks-title">Tasks</span>
       </div>
-      <NewTask />
+      <div className="tasks-list">
+        {tasks.map(task => {
+          return (
+            <div className="tasks-item" key={task.id}>
+              <div className="tasks-item-name">
+                <span>{task.name}</span>
+              </div>
+              <div className="tasks-item-trailing-content">
+                <div className="tasks-item-pomodoros">
+                  <span>0</span>
+                  <span>/</span>
+                  <span>{task.pomodoros}</span>
+                </div>
+                <div className="tasks-item-options-button"></div>
+              </div>
+            </div>
+          )
+        })}
+      </div>
+      <NewTask 
+        change={handleNewTaskChange} 
+        submit={handleNewTaskSubmit} 
+        pomodoros={pomodoros}
+        setPomodoros={setPomodoros}
+      />
     </div>
   )
 }
